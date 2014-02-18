c0fffabc1acfb5d434c37ee2580f5b85578e3099
Kirill Temnenkov <kirill@temnenkov.com>
2014-02-18 21:21:46 +0400

    команды покрыты тестами

--- jabot-logger/src/test/java/jabot/translator/commands/CommandParserImplTest.java
+++ jabot-logger/src/test/java/jabot/translator/commands/CommandParserImplTest.java
@@ -5,6 +5,7 @@ import jabot.translator.dto.TransUser;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,6 +29,66 @@ public class CommandParserImplTest {
         OperatorCmd cmd = parser.parse("!CREATE user@ttt.test");
         TestCase.assertNotNull(cmd);
         TestCase.assertEquals("user user@ttt.test:false", cmd.execute());
+    }
+
+    @Test
+    public void testParseDelete() throws Exception {
+        CommandParser parser = new CommandParserImpl(new TransusersWrapper() {
+            @Override
+            public void deleteIfExists(String jid) {
+            }
+        });
+        OperatorCmd cmd = parser.parse("!DELETE user@ttt.test");
+        TestCase.assertNotNull(cmd);
+    }
+
+    @Test
+    public void testUpdateTrue() throws Exception {
+
+        CommandParser parser = new CommandParserImpl(new TransusersWrapper() {
+            @Override
+            public TransUser updateIfExists(String jid, boolean enabled) {
+                TransUser u = new TransUser();
+                u.setEnabled(enabled);
+                u.setJid(jid);
+                return u;
+            }
+        });
+        OperatorCmd cmd = parser.parse("!UPDATE user@ttt.test 1");
+        TestCase.assertNotNull(cmd);
+        TestCase.assertEquals("updated user user@ttt.test:true", cmd.execute());
+    }
+
+    @Test
+    public void testUpdateFalse() throws Exception {
+
+        CommandParser parser = new CommandParserImpl(new TransusersWrapper() {
+            @Override
+            public TransUser updateIfExists(String jid, boolean enabled) {
+                TransUser u = new TransUser();
+                u.setEnabled(enabled);
+                u.setJid(jid);
+                return u;
+            }
+        });
+        OperatorCmd cmd = parser.parse("!UPDATE user@ttt.test 0");
+        TestCase.assertNotNull(cmd);
+        TestCase.assertEquals("updated user user@ttt.test:false", cmd.execute());
+    }
+
+    @Test
+    public void testList() throws Exception {
+        CommandParser parser = new CommandParserImpl(new TransusersWrapper() {
+            @Override
+            public List<String> getAll() {
+                List<String> data = new ArrayList<>();
+                data.add("dummy");
+                return data;
+            }
+        });
+        OperatorCmd cmd = parser.parse("!LIST");
+        TestCase.assertNotNull(cmd);
+        TestCase.assertEquals("dummy\n", cmd.execute());
     }
 
     private class TransusersWrapper implements Transusers {
